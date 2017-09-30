b019e4d490a57859da2a47716c685f0945b186c4
hobwekiva <4174bb0bb4d8c513566d@gmail.com>
2017-09-30 16:47:08 -0400

    More Iso functions.

--- src/main/scala/leibniz/Iso.scala
+++ src/main/scala/leibniz/Iso.scala
@@ -46,9 +46,28 @@ trait Iso[A, @sp B] { ab =>
     def from(a: A): B = ab.to(a)
     override val flip: Iso[A, B] = ab
   }
+
+  def and[I, J](ij: Iso[I, J]): Iso[(A, I), (B, J)] =
+    Iso.total(
+      { case (a, i) => (ab.to(a), ij.to(i)) },
+      { case (b, j) => (ab.from(b), ij.from(j)) })
+
+  def or[I, J](ij: Iso[I, J]): Iso[Either[A, I], Either[B, J]] =
+    Iso.total({
+      case Left(a) => Left(ab.to(a))
+      case Right(i) => Right(ij.to(i))
+    }, {
+      case Left(b) => Left(ab.from(b))
+      case Right(j) => Right(ij.from(j))
+    })
 }
 object Iso {
   def apply[A, B](implicit ab: Iso[A, B]): Iso[A, B] = ab
+
+  def total[A, B](ab: A => B, ba: B => A): Iso[A, B] = new Iso[A, B] {
+    def to(a: A): B = ab(a)
+    def from(b: B): A = ba(b)
+  }
 
   private[this] final case class Refl[A]() extends Iso[A, A] {
     def to(a: A): A = a
@@ -60,4 +79,66 @@ object Iso {
   implicit def id[A]: Iso[A, A] = anyIso.asInstanceOf[Iso[A, A]]
 
   implicit def flip[A, B](implicit ab: Iso[A, B]): Iso[B, A] = ab.flip
+
+  object Product {
+    type ⨂ [A, B] = (A, B)
+    type Id = Unit
+
+    final def associate[A, B, C]: Iso[A ⨂ (B ⨂ C), (A ⨂ B) ⨂ C] = total(
+      { case (a, (b, c)) => ((a, b), c) },
+      { case ((a, b), c) => (a, (b, c)) })
+
+    final def commute[A, B]: Iso[A ⨂ B, B ⨂ A] = total(
+      { case (a, b) => (b, a) },
+      { case (b, a) => (a, b) })
+
+    final def unit[A]: Iso[A, A ⨂ Id] = total(
+      { case a => (a, ()) },
+      { case (a, ()) => a })
+
+    final def first[A, B, C](iso: Iso[A, C]): Iso[A ⨂ B, C ⨂ B] =
+      iso and id
+
+    final def second[A, B, C](iso: Iso[B, C]): Iso[A ⨂ B, A ⨂ C] =
+      id and iso
+  }
+
+  object Coproduct {
+    type ⨂ [A, B] = Either[A, B]
+    type Id = Void
+
+    final def associate[A, B, C]: Iso[A ⨂ (B ⨂ C), (A ⨂ B) ⨂ C] =
+      total({
+        case Left(a) => Left(Left(a))
+        case Right(Left(b)) => Left(Right(b))
+        case Right(Right(c)) => Right(c)
+      }, {
+        case Left(Left(a)) => Left(a)
+        case Left(Right(b)) => Right(Left(b))
+        case Right(c) => Right(Right(c))
+      })
+
+    final def commute[A, B]: Iso[A ⨂ B, B ⨂ A] =
+      total({
+        case Left(a) => Right(a)
+        case Right(b) => Left(b)
+      },
+      {
+        case Right(a) => Left(a)
+        case Left(b) => Right(b)
+      })
+
+    final def unit[A]: Iso[A, A ⨂ Id] = total(
+      { case a => Left(a) },
+      {
+        case Left(a) => a
+        case Right(n) => Void.absurd(n)
+      })
+
+    final def first[A, B, C](iso: Iso[A, C]): Iso[A ⨂ B, C ⨂ B] =
+      iso or id
+
+    final def second[A, B, C](iso: Iso[B, C]): Iso[A ⨂ B, A ⨂ C] =
+      id or iso
+  }
 }
