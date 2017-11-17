4a9574efaa2c5c82017b33b04219f69cf3563390
hobwekiva <800ba735e867fc5457d5@gmail.com>
2017-11-17 15:22:47 -0500

    Got rid of asInstanceOf in TypeHolder (abusing null?).

--- src/main/scala/leibniz/TypeHolder.scala
+++ src/main/scala/leibniz/TypeHolder.scala
@@ -3,6 +3,8 @@ package leibniz
 object TypeHolder {
   trait Tag extends Any
 
-  @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
-  def apply[T]: TypeHolder[T] = ().asInstanceOf[TypeHolder[T]]
+  def apply[T]: TypeHolder[T] = {
+    object R { val r: TypeHolder[T] = identity(r) }
+    R.r
+  }
 }
